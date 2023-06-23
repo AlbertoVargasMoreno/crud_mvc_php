bc841dfd43db21eb000c0f8f8a2a35aaae44e21d
Another Normal User <earlybird@pop-os.localdomain>
2023-06-23 06:33:22 -0600

    cambios vista inicio de empleados

--- vistas/empleados/inicio.php
+++ vistas/empleados/inicio.php
@@ -1,4 +1,10 @@
 <!-- Esta es la tabla donde puedes ver la lista de empleados -->
+<div class="row justify-content-end g-2">
+    <div class="col-4">
+        <a href="?controldaor=paginas&accion=inicio" class="btn btn-success"> Ir a Inicio </a>
+        <a href="?controlador=empleados&accion=crear" class="btn btn-success"> Agregar Empleado </a>
+    </div>
+</div>
 <table class="table table-bordered">
     <thead>
         <tr>
@@ -16,12 +22,17 @@
                 <th> <?php echo $empleado->correo; ?> </th>
                 <th>
                     <div>
-		        <a href="?controlador=empleados&accion=editar&id=<?php echo $empleado->id; ?>" class="btn"> Editar </a>
-			<a href="?controlador=empleados&accion=borrar&id=<?php echo $empleado->id; ?>" class="btn"> Borrar </a>
+                        <a href="?controlador=empleados&accion=editar&id=<?php echo $empleado->id; ?>" 
+                            class="btn"> 
+                            Editar 
+                        </a>
+                        <a href="?controlador=empleados&accion=borrar&id=<?php echo $empleado->id; ?>" 
+                            class="btn"> 
+                            Borrar 
+                        </a>
                     </div>
                 </th>
             </tr>
         <?php } ?>
     </tbody>
-    <a href="?controlador=empleados&accion=crear" class="btn btn-success"> Agregar Empleado </a>
 </table>
